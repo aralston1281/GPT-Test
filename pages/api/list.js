297ea4938fd5df2bf58f7e5d458bf5cfe7e86dc0
aralston1281 <84287098+aralston1281@users.noreply.github.com>
2025-04-02 18:51:32 -0400

    Delete pages/api directory

--- pages/api/list.js
+++
@@ -1,8 +0,0 @@
-// pages/api/list.js
-import { redis } from '../../lib/redis';
-
-export default async function handler(_, res) {
-  const keys = await redis.keys('planner:*');
-  const names = keys.map((k) => k.replace('planner:', ''));
-  res.status(200).json(names);
-}
