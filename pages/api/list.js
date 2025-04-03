43fc170a6313cc4cf9bca3890ba0a6cc4e251ea1
aralston1281 <84287098+aralston1281@users.noreply.github.com>
2025-04-02 20:58:06 -0400

    Delete pages directory

--- pages/api/list.js
+++
@@ -1,7 +0,0 @@
-import { redis } from '../../lib/redis';
-
-export default async function handler(_, res) {
-  const keys = await redis.keys('planner:*');
-  const names = keys.map(k => k.replace('planner:', ''));
-  res.status(200).json(names);
-}
--- pages/index.js
+++
@@ -1,6 +0,0 @@
-import LoadDistributionPlanner from '../LoadDistributionPlanner';
-import config from '../config.json';
-
-export default function Home() {
-  return <LoadDistributionPlanner config={config} />;
-}
